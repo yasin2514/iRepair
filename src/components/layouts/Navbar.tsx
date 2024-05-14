a28232b08eac26c12b6c329960d81fe6a5b4c6ff
yasin2514 <yasinkhanrabbi1999@gmail.com>
2024-05-14 23:33:14 +0600

    framer motion animation added into hero text section

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <header className=" h-16">
       <nav className="w-full max-w-[1230px] h-full mx-auto flex justify-between items-center px-5">
-        <span className="text-3xl">iRepair</span>
+        <span className="text-3xl font-semibold">iRepair</span>
         <ul className="space-x-5">
           <NavLink to={"/"}>Home</NavLink>
           <NavLink to={"/about"}>About</NavLink>
--- src/pages/home/HeroSection.tsx
+++ src/pages/home/HeroSection.tsx
@@ -1,24 +1,53 @@
 import Container from "@/components/Container";
 import { Button } from "@/components/ui/button";
 import macBook from "@/assets/images/macbook-exposed.png";
+import { delay, motion } from "framer-motion";
+
+const intro = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      staggerChildren: 0.25,
+      delayChildren: 0.5,
+    },
+  },
+};
+const introChildren = {
+  hidden: { opacity: 0, y: -200 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.5, type: "spring", bounce: 0.5 },
+  },
+};
 
 const HeroSection = () => {
   return (
     <Container className="grid grid-cols-1 lg:grid-cols-2 h-[calc(100vh-64px)] place-content-center">
-      <div>
-        <h1 className="text-4xl lg:text-8xl font-bold text-nowrap">
+      <motion.div variants={intro} initial="hidden" animate="visible">
+        <motion.h1
+          className="text-4xl lg:text-8xl font-bold text-nowrap"
+          variants={introChildren}
+        >
           <span className="text-gray">Don't worry.</span>
           <br />
           <span>We'll fix it.</span>
-        </h1>
-        <p className="text-dark-gray max-w-[50ch] mt-10 mb-6">
+        </motion.h1>
+        <motion.p
+          className="text-dark-gray max-w-[50ch] mt-10 mb-6"
+          variants={introChildren}
+        >
           Welcome to <span className="font-semibold text-black"> iRepair</span>,
           your one-stop place for all kinds of
-          <span className="font-semibold text-black"> Macbook repairs</span> and
+          <span className="font-semibold text-black"> MacBook repairs</span> and
           diagnostics.
-        </p>
-        <Button>Book a Service</Button>
-      </div>
+        </motion.p>
+        <motion.div variants={introChildren}>
+          <Button>Book a Service</Button>
+        </motion.div>
+      </motion.div>
       <div className=" w-3/4 md:w-2/4 lg:w-full mx-auto mt-10 lg:mt-0 flex items-center justify-center">
         <img
           className="-rotate-[35deg] h-[90%] object-contain"
